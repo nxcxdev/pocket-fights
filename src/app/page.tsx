34b7abdcf7cd3dded290c73220ab18a820cafbd8
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2024-03-13 00:12:31 +0100

    Created header component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/header/header";
 
 export default function Home() {
 	return (
-    <main>
-      
-    </main>
-  );
+		<main className={styles.main}>
+			<Header />
+		</main>
+	);
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "./styles.module.css";
+export default function Header() {
+	return (
+		<header>
+			<nav className={styles.navbar}>
+				<section className={styles.linkSection}>
+					<Link href="/home" className={styles.link}>
+						Home
+					</Link>
+					<Link href="/collection" className={styles.link}>
+						Collection
+					</Link>
+				</section>
+				<Link className={styles.profile} href={"/profile"}>
+					<p className={styles.profileName}>kittydestroyer</p>
+					<img src="" alt="" className={styles.profilePicture} />
+				</Link>
+			</nav>
+		</header>
+	);
+}
